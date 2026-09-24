d6ad161b1ed3d3ec85ceb619889394e317dcc926
Alex Paquette <alexandre.d.paquette@gmail.com>
2026-09-23 21:25:52 -0300

    Implement level switching

--- Globals/GameManager.cs
+++ Globals/GameManager.cs
@@ -4,9 +4,13 @@ using System;
 public partial class GameManager : Node
 {
 	public static GameManager Instance { get; private set; }
+	public static PackedScene MainScene {get; private set;} = GD.Load<PackedScene>("res://Scenes/Main/Main.tscn");
+	public static PackedScene LevelScene {get; private set;} = GD.Load<PackedScene>("res://Scenes/LevelBase/LevelBase.tscn");
 	
 	public override void _Ready()
 	{
 		Instance = this;
 	}
+
+	public static void LoadScene(PackedScene scene) => Instance.GetTree().ChangeSceneToPacked(scene);
 }
